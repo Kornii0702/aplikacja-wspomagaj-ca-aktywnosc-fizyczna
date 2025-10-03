ae089f9163d6183da37a74170603b33a4fe54b23
Kornii0702 <kornel.myczko@gmail.com>
2025-10-03 18:57:24 +0200

    dependencies added

--- inzynierka/app/build.gradle.kts
+++ inzynierka/app/build.gradle.kts
@@ -41,7 +41,12 @@ android {
 }
 
 dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.4.0")) // use the latest BoM
+    implementation("com.google.firebase:firebase-auth-ktx")
 
+    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.compose.ui:ui:1.7.2")
+    implementation("androidx.compose.material:material:1.7.2")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -51,6 +56,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.firebase.auth)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
