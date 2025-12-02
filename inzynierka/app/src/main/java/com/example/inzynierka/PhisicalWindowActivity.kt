d524793c073fa0abcd6afd9417fad852bdd4bee3
Kornii0702 <kornel.myczko@gmail.com>
2025-12-02 09:57:34 +0100

    firestore fixed on phone. Added PhisicalWindowActivity.kt

---
+++ inzynierka/app/src/main/java/com/example/inzynierka/PhisicalWindowActivity.kt
@@ -0,0 +1,20 @@
+package com.example.inzynierka
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PhisicalWindowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_phisical_window)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
